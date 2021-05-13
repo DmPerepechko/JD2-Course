449caf1b068eb90164bdf09f559edd691d918762
DmPerepechko <dimitriper8@gmail.com>
2021-05-13 14:27:14 +0300

    Added SQL scripts file for database creation

---
+++ task5/src/ScriptsPeople.sql
@@ -0,0 +1,22 @@
+DROP SCHEMA IF EXISTS `dao_task`;
+CREATE SCHEMA IF NOT EXISTS `dao_task` DEFAULT CHARACTER SET utf8;
+DROP TABLE IF EXISTS `dao_task`.`address`;
+CREATE TABLE IF NOT EXISTS `dao_task`.`address`
+(
+    id     int auto_increment
+        primary key,
+    street varchar(100) null,
+    house  int          null
+);
+DROP TABLE IF EXISTS `dao_task`.`people`;
+CREATE TABLE IF NOT EXISTS `dao_task`.`people`
+(
+    id      int auto_increment
+        primary key,
+    name    varchar(100) null,
+    surname varchar(200) null,
+    age     int          null
+);
+
+truncate table dao_task.address;
+truncate table dao_task.people;
